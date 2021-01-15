b6ebe9f41f652434c5656016bdaa1a61ff89f7eb
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-15 18:52:02 -0500

    add row component

---
+++ src/Components/TableRow.jsx
@@ -0,0 +1,10 @@
+import TableCell from './TableCell';
+import '../App.css';
+
+export default function TableRow (props) {
+  return (
+    <div className="row">
+      {Array(props.numCols).fill(<TableCell />)}
+    </div>
+  )
+}
